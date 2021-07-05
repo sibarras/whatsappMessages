e415896a3755ed61984fd2ab6de427568de29cfc
Samuel Ibarra <ing.samuelibarra@gmail.com>
2021-07-05 15:59:35 -0500

    adding main

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from twilio_whastapp import whatsapp_message
 from tkinter import simpledialog
 
 
-whatsapp_message('ahora que')
+whatsapp_message('Hola Samuel, estas volviendo a utilizar la API de twilio.')
